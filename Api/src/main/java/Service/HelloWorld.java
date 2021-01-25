c0359086990c0558d3a0b738d91451ea14b5ea49
DanielFRengifo <73077115+DanielFRengifo@users.noreply.github.com>
2021-01-25 10:38:00 -0500

    Implementation: Endpoint Open
    Implementar el endpoint que abre una ruleta determinada

--- Api/src/main/java/Service/HelloWorld.java
+++ Api/src/main/java/Service/HelloWorld.java
@@ -4,8 +4,30 @@ import static spark.Spark.*;
 
 public class HelloWorld 
 {
-	public static void main(String[] args) {
-        post("/rule", (request, response) -> "Hello Worlddd");
-    }
+	public static void main(String[] args) 
+	{
+        	ArrayList<Ruleta> ruletas = new ArrayList<Ruleta>(); 
+		post("/Ruleta", (request, response) -> 
+		{
+			Ruleta ruleta = new Ruleta(ruletas.size());
+			ruletas.add(ruleta); 
+			return String.valueOf(ruleta.getId());
+		});
+		
+		post("/OpenRuleta", (request, response) -> 
+		{	
+			try
+			{
+			int id = Integer.parseInt(request.body().replaceAll("[^\\d.]", ""));
+			Ruleta ruleta = ruletas.get(id); 
+			ruleta.setOpen(true);
+			return "TRUE";
+			}
+			catch (Exception e)
+			{
+				return "FALSE";
+			}
+		});
+	}
 
 }
